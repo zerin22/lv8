d46e742f008e2436514e874d10a42122472221e1
Tajrin Zerin <tajrin.ini@gmail.com>
2021-07-31 16:33:46 +0600

    Homepage, About, Contact+commmon navbar with bootstrp & name routing learning

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<section class="bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="nav-bar text-center">
+                    <a href="{{ route('homepage') }}">Home</a>
+                    <a href="{{ route('about') }}">About</a>
+                    <a href="{{ route('contact') }}">Contact</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
 
-Route::get('about', function(){
-    return 'This is about page';
-});
+Route::get('about-us', function(){
+    return view('about');
+})->name('about');
+
+Route::get('contact-us', function(){
+    return view('contact');
+})->name('contact');
